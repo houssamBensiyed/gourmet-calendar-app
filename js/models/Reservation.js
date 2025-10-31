1fb3d72ba7790909a4ab1fd6df9b63a01f868207
Houssam Bensiyed <med.ab02017@gmail.com>
2025-10-31 10:30:45 +0100

    implement Reservation class

---
+++ js/models/Reservation.js
@@ -0,0 +1,35 @@
+export class Reservation {
+  constructor(data) {
+    this.id = data.id || this.generateId();
+    this.clientName = Date.clientName;
+    this.day = data.day;
+    this.date = data.date;
+    this.startTime = data.startTime;
+    this.endTime = data.endTime;
+    this.numberOfPeople = data.numberOfPeople;
+    this.type = data.type;
+    this.notes = data.notes || "";
+  }
+
+  generateId() {
+    return `res_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      clientName: this.clientName,
+      day: this.day,
+      date: this.date,
+      startTime: this.startTime,
+      endTime: this.endTime,
+      numberOfPeople: this.numberOfPeople,
+      type: this.type,
+      notes: this.notes,
+    };
+  }
+
+  static fromJSON(data) {
+    return new Reservation(data);
+  }
+}
