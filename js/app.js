7e07f854a9b98f0b55cea9d24f63821988689ea6
Houssam Bensiyed <med.ab02017@gmail.com>
2025-10-31 16:34:07 +0100

    instantiate the calendarController

---
+++ js/app.js
@@ -0,0 +1,7 @@
+import { CalendarController } from "./controllers/CalendarController.js";
+
+let calendarController;
+
+document.addEventListener("DOMContentLoaded", () => {
+  calendarController = new CalendarController();
+});
