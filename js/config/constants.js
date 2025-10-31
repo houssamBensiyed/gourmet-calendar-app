44dfbe4d3377ac2369b355660c9e7a1024c649bf
Houssam Bensiyed <med.ab02017@gmail.com>
2025-10-31 10:18:07 +0100

    add config object

---
+++ js/config/constants.js
@@ -0,0 +1,11 @@
+export default CONFIG = {
+  START_HOUR: 8,
+  END_HOUR: 22,
+  TOAST_DURATION: 3000,
+  SEARCH_DEBOUNCE: 300,
+  MIN_NAME_LENGTH: 2,
+  MIN_PEOPLE: 1,
+  MAX_PEOPLE: 100,
+  MIN_DURATION_MINUTES: 30,
+  STORAGE_KEY: "reservex_reservations",
+};
